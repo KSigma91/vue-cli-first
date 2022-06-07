930daf866749b069cb7f428196ed1b15a627011a
Andrea Guidotti <andrea19912014@gmail.com>
2022-06-07 17:32:04 +0200

    nuovo componente

---
+++ src/components/myComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <h1>La mia prima app con Vue CLI!</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name : 'myComponent',
+        props : {
+            msg : String
+        }
+    }
+</script>
+
+<style scoped>
+    h1 {
+        margin: 40px 0 0;
+        color: deepskyblue;
+    }
+</style>
